324c0e7fa0b9b1948275807804f0f0280d02421b
aki <lanercifang@outlook.com>
2023-05-12 11:12:12 +0800

    header making

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,36 @@ import Head from 'next/head'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-      >
-          <Head>
-          <title>UI/UX</title>
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        >
+            <Head>
+                <title>UI/UX</title>
                 <link rel="icon" href="/favicon.ico" />
-          </Head>
+            </Head>
 
-    </main>
-  )
+            <div className={styles.header}>
+                <div className={styles.headone}>
+                    <div className={styles.logos}>
+                        <div className={styles.lab}></div>
+                        <div className={styles.logo}></div>
+                    </div>
+                    <div className={styles.sections}></div>
+                    <div className={styles.index}></div>
+                </div>
+                <div className={styles.headtwo}></div>
+                <div className={styles.headthree}></div>
+                <div className={styles.headhide}></div>
+            </div>
+
+            {/* 一个分割线 */}
+
+            <div className={styles.text}>
+                
+            </div>
+
+
+        </main>
+    )
 }
